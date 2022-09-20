53273be401209c2b148d23512eeef38bcefa1b77
Jorge Villafuerte <jorge_123_386@hotmail.es>
2022-09-20 06:56:34 -0400

    Removed unused imported library.

--- src/main/java/net/jorgev96/jroleitems/item/ModItems.java
+++ src/main/java/net/jorgev96/jroleitems/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.jorgev96.jroleitems.item;
 
 import net.jorgev96.jroleitems.JRoleItems;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,16 +13,16 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, JRoleItems.MOD_ID);
 
     public static final RegistryObject<Item> COPPER_COIN = ITEMS.register("copper_coin",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.JORGE_ROLE_ITEMS_TAB)));
 
     public static final RegistryObject<Item> SILVER_COIN = ITEMS.register("silver_coin",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.JORGE_ROLE_ITEMS_TAB)));
 
     public static final RegistryObject<Item> GOLD_COIN = ITEMS.register("gold_coin",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.JORGE_ROLE_ITEMS_TAB)));
 
     public static final RegistryObject<Item> DIAMOND_COIN = ITEMS.register("diamond_coin",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.JORGE_ROLE_ITEMS_TAB)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
