1c8f4fba51603c81b6bf61026d8223f564f8ed6f
Jorge Villafuerte <jorge_123_386@hotmail.es>
2022-09-20 06:57:21 -0400

    Updated code to allow the new decorative blocks.

--- src/main/java/net/jorgev96/jroleitems/JRoleItems.java
+++ src/main/java/net/jorgev96/jroleitems/JRoleItems.java
@@ -1,23 +1,15 @@
 package net.jorgev96.jroleitems;
 
 import com.mojang.logging.LogUtils;
+import net.jorgev96.jroleitems.block.ModBlocks;
 import net.jorgev96.jroleitems.item.ModItems;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(JRoleItems.MOD_ID)
@@ -31,6 +23,7 @@ public class JRoleItems
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
