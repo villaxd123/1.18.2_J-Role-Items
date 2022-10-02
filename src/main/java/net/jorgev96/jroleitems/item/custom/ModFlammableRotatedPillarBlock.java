190f23e1b49f4b39a4594a67283f3f2de5616e03
Jorge Villafuerte <jorge_123_386@hotmail.es>
2022-10-02 11:23:34 -0400

    Added new class for the net type of wood.

---
+++ src/main/java/net/jorgev96/jroleitems/item/custom/ModFlammableRotatedPillarBlock.java
@@ -0,0 +1,9 @@
+package net.jorgev96.jroleitems.item.custom;
+
+import net.minecraft.world.level.block.RotatedPillarBlock;
+
+public class ModFlammableRotatedPillarBlock extends RotatedPillarBlock {
+    public ModFlammableRotatedPillarBlock(Properties pProperties) {
+        super(pProperties);
+    }
+}
