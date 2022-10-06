7f278998e5bee2fb731b9eb0380e982ac3196bec
Jorge Villafuerte <jorge_123_386@hotmail.es>
2022-10-06 12:15:41 -0400

    Added the tooltip for the first food.

--- src/main/java/net/jorgev96/jroleitems/item/ModItems.java
+++ src/main/java/net/jorgev96/jroleitems/item/ModItems.java
@@ -2,11 +2,20 @@ package net.jorgev96.jroleitems.item;
 
 import net.jorgev96.jroleitems.JRoleItems;
 import net.jorgev96.jroleitems.item.custom.RollDiceItem;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ModItems {
 
@@ -33,6 +42,19 @@ public class ModItems {
 
     public static final RegistryObject<Item> PURPUMETRINE_CHARGED = ITEMS.register("purpumetrine_charged",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.JORGE_ROLE_ITEMS_TAB)));
+
+    public static final RegistryObject<Item> PIZZA_A = ITEMS.register("pizza_a",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.JORGE_ROLE_ITEMS_TAB)
+                    .food(ModFoods.PIZZA_A)){
+                @Override
+                public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+                    if (Screen.hasShiftDown()){
+                        pTooltipComponents.add(new TranslatableComponent("tooltip.jroleitems.pizza_a.tooltip.shift"));
+                    } else {
+                        pTooltipComponents.add(new TranslatableComponent("tooltip.jroleitems.pizza_a.tooltip"));
+                    }
+                }
+            });
 
     public static final RegistryObject<Item> DICE = ITEMS.register("dice",
             () -> new RollDiceItem(new Item.Properties().tab(ModCreativeModeTab.JORGE_ROLE_ITEMS_TAB)
