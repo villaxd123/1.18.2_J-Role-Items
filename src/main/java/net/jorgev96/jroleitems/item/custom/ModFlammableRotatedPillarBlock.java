07b8a5e6adda1116b4f7c4ef0ce7f9f75f1a0e30
Jorge Villafuerte <jorge_123_386@hotmail.es>
2022-10-25 08:28:32 -0400

    Added the Flammable & Rotated Blocks Class.

--- src/main/java/net/jorgev96/jroleitems/item/custom/ModFlammableRotatedPillarBlock.java
+++
@@ -1,9 +0,0 @@
-package net.jorgev96.jroleitems.item.custom;
-
-import net.minecraft.world.level.block.RotatedPillarBlock;
-
-public class ModFlammableRotatedPillarBlock extends RotatedPillarBlock {
-    public ModFlammableRotatedPillarBlock(Properties pProperties) {
-        super(pProperties);
-    }
-}
