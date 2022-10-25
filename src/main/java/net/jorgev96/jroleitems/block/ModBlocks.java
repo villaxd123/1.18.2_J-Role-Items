0d2a4a7ac9c58abb3cf893329077741ae657523c
Jorge Villafuerte <jorge_123_386@hotmail.es>
2022-10-25 09:21:34 -0400

    A little visual change.

--- src/main/java/net/jorgev96/jroleitems/block/ModBlocks.java
+++ src/main/java/net/jorgev96/jroleitems/block/ModBlocks.java
@@ -29,6 +29,86 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, JRoleItems.MOD_ID);
 
+    public static final RegistryObject<Block> WHITE_TILE_BLOCK = registerBlock("white_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> ORANGE_TILE_BLOCK = registerBlock("orange_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> MAGENTA_TILE_BLOCK = registerBlock("magenta_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> LIGHTBLUE_TILE_BLOCK = registerBlock("lightblue_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> YELLOW_TILE_BLOCK = registerBlock("yellow_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> LIME_TILE_BLOCK = registerBlock("lime_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> PINK_TILE_BLOCK = registerBlock("pink_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> GRAY_TILE_BLOCK = registerBlock("gray_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> LIGHTGRAY_TILE_BLOCK = registerBlock("lightgray_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> CYAN_TILE_BLOCK = registerBlock("cyan_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> PURPLE_TILE_BLOCK = registerBlock("purple_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> BLUE_TILE_BLOCK = registerBlock("blue_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> BROWN_TILE_BLOCK = registerBlock("brown_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> GREEN_TILE_BLOCK = registerBlock("green_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> RED_TILE_BLOCK = registerBlock("red_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> BLACK_TILE_BLOCK = registerBlock("black_tile_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> PURPUMETRINE_BLOCK = registerBlock("purpumetrine_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> RAW_PURPUMETRINE_BLOCK = registerBlock("raw_purpumetrine_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> PURPUMETRINE_ORE = registerBlock("purpumetrine_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> DEEPSLATE_PURPUMETRINE_ORE = registerBlock("deepslate_purpumetrine_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
+
     public static final RegistryObject<Block> ANCESTRAL_LOG_BLUE = registerBlock("ancestral_log_blue",
             () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)),
             ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
@@ -130,86 +210,6 @@ public class ModBlocks {
                     return 5;
                 }
             }, ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> WHITE_TILE_BLOCK = registerBlock("white_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> ORANGE_TILE_BLOCK = registerBlock("orange_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> MAGENTA_TILE_BLOCK = registerBlock("magenta_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> LIGHTBLUE_TILE_BLOCK = registerBlock("lightblue_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> YELLOW_TILE_BLOCK = registerBlock("yellow_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> LIME_TILE_BLOCK = registerBlock("lime_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> PINK_TILE_BLOCK = registerBlock("pink_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> GRAY_TILE_BLOCK = registerBlock("gray_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> LIGHTGRAY_TILE_BLOCK = registerBlock("lightgray_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> CYAN_TILE_BLOCK = registerBlock("cyan_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> PURPLE_TILE_BLOCK = registerBlock("purple_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> BLUE_TILE_BLOCK = registerBlock("blue_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> BROWN_TILE_BLOCK = registerBlock("brown_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> GREEN_TILE_BLOCK = registerBlock("green_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> RED_TILE_BLOCK = registerBlock("red_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> BLACK_TILE_BLOCK = registerBlock("black_tile_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> PURPUMETRINE_BLOCK = registerBlock("purpumetrine_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> RAW_PURPUMETRINE_BLOCK = registerBlock("raw_purpumetrine_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> PURPUMETRINE_ORE = registerBlock("purpumetrine_ore",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
-
-    public static final RegistryObject<Block> DEEPSLATE_PURPUMETRINE_ORE = registerBlock("deepslate_purpumetrine_ore",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTab.JORGE_ROLE_BLOCKS_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name,
                                                                      Supplier<T> block,
