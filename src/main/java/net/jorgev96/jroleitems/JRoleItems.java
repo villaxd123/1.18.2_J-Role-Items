960cdb05633be2bfd909deef887a460e44e062df
Jorge Villafuerte <jorge_123_386@hotmail.es>
2022-10-27 14:51:19 -0400

    Added a rendering method.

--- src/main/java/net/jorgev96/jroleitems/JRoleItems.java
+++ src/main/java/net/jorgev96/jroleitems/JRoleItems.java
@@ -3,10 +3,13 @@ package net.jorgev96.jroleitems;
 import com.mojang.logging.LogUtils;
 import net.jorgev96.jroleitems.block.ModBlocks;
 import net.jorgev96.jroleitems.item.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -26,9 +29,28 @@ public class JRoleItems
         ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_DOOR_BLUE.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_DOOR_ORANGE.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_DOOR_PURPLE.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_TRAPDOOR_BLUE.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_TRAPDOOR_ORANGE.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_TRAPDOOR_PURPLE.get(), RenderType.translucent());
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_LEAVES_BLUE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_LEAVES_ORANGE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_LEAVES_PURPLE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_SAPLING_BLUE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_SAPLING_ORANGE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ANCESTRAL_SAPLING_PURPLE.get(), RenderType.cutout());
+
+        // ModItemProperties.addCustomItemProperties();
     }
 
     private void setup(final FMLCommonSetupEvent event) {
